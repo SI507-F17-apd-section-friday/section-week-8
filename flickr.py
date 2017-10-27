675697235fa9c58c4bcb92482c8d96a152318f87
Anand Doshi <hello@apd.is>
2017-10-27 09:18:26 -0400

    changes

--- flickr.py
+++ flickr.py
@@ -25,7 +25,7 @@ def params_unique_combination(baseurl, params_d, private_keys=["api_key"]):
             res.append("{}-{}".format(k, params_d[k]))
     return baseurl + "_".join(res)
 
-def search_flickr_by_tags(tags):
+def search_flickr(additional_params):
     if not FLICKR_API_KEY:
         raise Exception('Flickr API Key is missing!')
 
@@ -38,6 +38,9 @@ def search_flickr_by_tags(tags):
         "per_page": 10,
         "nojsoncallback": 1
     }
+
+    for key, value in additional_params.items():
+        params_diction[key] = value
 
     unique_ident = params_unique_combination(baseurl,params_diction)
     if unique_ident in CACHE_DICTION:
